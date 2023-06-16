d69a857a55abd0120abb6eb91b6d6ecef5dfc77a
Fan Dev <134129030+FanDev404@users.noreply.github.com>
2023-06-16 23:30:04 +0700

    Add files via upload

--- system/commands/set_prefix.js
+++ system/commands/set_prefix.js
@@ -4,20 +4,20 @@ module.exports = {
     isSewa: true,
     isOwner: true,
     callback: async ({ m }) => {
-        if (m.args[0] == "prefix" || m.args[0] == "1") {
-        if (m.setPrefix == "prefix") return m.reply("Sudah active")
-        db.settings[m.botNumber].setprefix = "prefix"
-        await m.reply("Success mengganti prefix ke prefix")
-        } else if (m.args[0] == "noprefix" || m.args[0] == "2") {
-        if (m.setPrefix == "noprefix") return m.reply("Sudah active")
-        db.settings[m.botNumber].setprefix = "noprefix"
-        await m.reply("Success mengganti prefix ke noprefix")
-        } else if (m.args[0] == "allprefix" || m.args[0] == "3") {
-        if (m.setPrefix == "allprefix") return m.reply("Sudah active")
-        db.settings[m.botNumber].setprefix = "allprefix"
-        await m.reply("Success mengganti prefix ke allprefix")
+        if (m.args[0] == "yes" || m.args[0] == "1") {
+        if (m.setPrefix == "yes") return m.reply("Sudah active")
+        db.settings[m.botNumber].setprefix = "yes"
+        await m.reply("Success mengganti prefix ke yes")
+        } else if (m.args[0] == "noo" || m.args[0] == "2") {
+        if (m.setPrefix == "noo") return m.reply("Sudah active")
+        db.settings[m.botNumber].setprefix = "noo"
+        await m.reply("Success mengganti prefix ke noo")
+        } else if (m.args[0] == "all" || m.args[0] == "3") {
+        if (m.setPrefix == "all") return m.reply("Sudah active")
+        db.settings[m.botNumber].setprefix = "all"
+        await m.reply("Success mengganti prefix ke all")
         } else {
-        m.reply("\`\`\`「 SETTINGS PREFIX BOT 」\`\`\`\n\n1. prefix\n2. noprefix\n3. allprefix")
+        m.reply("\`\`\`「 SETTINGS PREFIX BOT 」\`\`\`\n\n1. yes\n2. noo\n3. all")
         }
     }
 }
